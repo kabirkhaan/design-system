74374da3d0a083880c7d14097e9ced1b8de66b84
Kabir Khan <muhammad-kabir.khan@sixt.de>
2022-07-22 12:14:30 +0200

    Added compose support and remove web

--- build.js
+++ build.js
@@ -6,56 +6,109 @@ function getStyleDictionaryConfig(brand, platform) {
     return {
         "source": [
             `tokens/brands/${brand}/*.json`,
-            "tokens/global/**/*.json"
+            `tokens/**/**/*.json`,
         ],
         "platforms": {
-            "web": {
-                "transformGroup": "web",
-                "buildPath": `build/web/${brand}/`,
-                "files": [{
-                    "destination": "tokens.scss",
-                    "format": "scss/variables"
-                }]
-            },
             "android": {
+                "buildPath": `build/${brand}/android/`,
                 "transformGroup": "android",
-                "buildPath": `build/android/${brand}/`,
                 "files": [{
-                        "destination": "tokens.colors.xml",
-                        "format": "android/colors"
-                    }, {
-                        "destination": "tokens.dimens.xml",
-                        "format": "android/dimens"
-                    }, {
-                        "destination": "tokens.font_dimens.xml",
-                        "format": "android/fontDimens"
+                  "destination": "tokens_colors.xml",
+                  "format": "android/colors"
+                },{
+                  "destination": "tokens_font_dimens.xml",
+                  "format": "android/fontDimens"
+                },{
+                  "destination": "tokens_dimens.xml",
+                  "format": "android/dimens"
+                },{
+                  "destination": "tokens_integers.xml",
+                  "format": "android/integers"
+               },{
+                  "destination": "tokens_strings.xml",
+                  "format": "android/strings"
+                },{
+                  "destination":"Color.kt",
+                  "format": "compose/object",
+                  "className":"Color",
+                  "filter":{
+                     "attributes":{
+                        "category":"font"
+                     }
+                  }
+               },{
+                  "destination":"Size.kt",
+                  "format": "compose/object",
+                  "className":"Size",
+                  "type":"float",
+                  "filter":{
+                     "attributes":{
+                        "category":"size"
+                     }
+                  }
+               },{
+                  "destination":"Fonts.kt",
+                  "format": "compose/object",
+                  "className":"FontName",
+                  "filter":{
+                     "attributes":{
+                        "category":"font"
+                     }
+                  }
+               },{
+                 "destination":"Components.kt",
+                 "format": "compose/object",
+                 "className":"Components",
+                 "filter":{
+                    "attributes":{
+                       "category":"component"
                     }
-                ]
+                 }
+              }
+              ]
             },
+            
             "ios": {
                 "transformGroup": "ios-swift-separate",
-                "buildPath": `build/ios/${brand}/`,
-                "files": [{
-                        "destination": "Color.swift",
-                        "format": "ios-swift/enum.swift",
-                        "className": "Color",
-                        "filter": {
-                            "attributes": {
-                                "category": "color"
-                            }
+                "buildPath": `build/${brand}/ios/`,
+                "files":[{
+                     "destination":"Color.swift",
+                     "format":"ios-swift/enum.swift",
+                     "className":"Color",
+                     "filter":{
+                        "attributes":{
+                           "category":"font"
                         }
-                    },
-                    {
-                        "destination": "Size.swift",
-                        "format": "ios-swift/enum.swift",
-                        "className": "Size",
-                        "type": "float",
-                        "filter": {
-                            "attributes": {
-                                "category": "size"
-                            }
+                     }
+                  },{
+                     "destination":"Size.swift",
+                     "format":"ios-swift/enum.swift",
+                     "className":"Size",
+                     "type":"float",
+                     "filter":{
+                        "attributes":{
+                           "category":"size"
                         }
-                    },
+                     }
+                  },{
+                     "destination":"Fonts.swift",
+                     "format":"ios-swift/enum.swift",
+                     "className":"FontName",
+                     "filter":{
+                        "attributes":{
+                           "category":"font"
+                        }
+                     }
+                  },{
+                    "destination":"Components.swift",
+                    "format":"ios-swift/enum.swift",
+                    "className":"Components",
+                    "filter":{
+                       "attributes":{
+                          "category":"component"
+                       }
+                    }
+                 }
                 ]
             },
         }
@@ -67,7 +120,7 @@ console.log('Build started...');
 // PROCESS THE DESIGN TOKENS FOR THE DIFFERENT BRANDS AND PLATFORMS
 
 ['brand1', 'brand2', 'brand3'].map(function(brand) {
-    ['web', 'ios', 'android'].map(function(platform) {
+    ['ios', 'android'].map(function(platform) {
 
         console.log('\n==============================================');
         console.log(`\nProcessing: [${platform}] [${brand}]`);
